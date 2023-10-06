e7e4577bbe96f59bd2069a896df2fbcea4f0bcec
ebrahim-ramadan <ramadanebrahim791@gmail.com>
2023-10-06 06:04:07 +0200

    provided some consistency with debouncing

--- app/page.js
+++ app/page.js
@@ -1,16 +1,26 @@
 'use client'
 import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
+import { debounce } from 'lodash'; // Import debounce from lodash
 
 const Base_URL = 'https://placekitten.com';
 
 export default function Home() {
   const [width, setWidth] = useState(400); // Default width
   const [height, setHeight] = useState(300); // Default height
-  const [imageSrc, setImageSrc] = useState(`${Base_URL}/${width}/${height}`); 
+  const [imageSrc, setImageSrc] = useState(`${Base_URL}/${width}/${height}`); // Image source
+
+  // Use debounced functions for slider onChange handlers
+  const handleWidthChange = debounce((value) => {
+    setWidth(value);
+  }, 200); 
+
+  const handleHeightChange = debounce((value) => {
+    setHeight(value);
+  }, 200); 
 
   useEffect(() => {
-    // Update the image source when width or height changes here rather than updating directly in image src(bad-doesnt hive a fuck- behavior), The src attribute of the Image component is set to imageSrc, which is dynamically updated when the slider values change. This ensures that the image loading waits for the new pictures to be fetched based on the updated slider values.
+    // Update the image source when width or height changes
     setImageSrc(`${Base_URL}/${width}/${height}`);
   }, [width, height]);
 
@@ -19,23 +29,23 @@ export default function Home() {
       <div>
         <label
           htmlFor="customRangeWidth"
-          className="inline-block text-neutral-700 dark:text-neutral-200"
+          className="mb-2 inline-block text-neutral-700 dark:text-neutral-200"
         >
           Width
         </label>
         <input
           type="range"
-          className="mb-4 transparent h-[4px] w-full cursor-pointer appearance-none border-transparent bg-neutral-200 dark:bg-neutral-600"
+          className="transparent h-[4px] w-full cursor-pointer appearance-none border-transparent bg-neutral-200 dark:bg-neutral-600"
           min="50"
           max="700"
           step="10"
           id="customRangeWidth"
           value={width}
-          onChange={(event) => setWidth(event.target.value)}
+          onChange={(event) => handleWidthChange(event.target.value)}
         />
         <label
           htmlFor="width"
-          className="inline-block text-neutral-700 dark:text-neutral-200"
+          className="mb-2 inline-block text-neutral-700 dark:text-neutral-200"
         >
           Height
         </label>
@@ -47,19 +57,23 @@ export default function Home() {
           step="10"
           id="height"
           value={height}
-          onChange={(event) => setHeight(event.target.value)}
+          onChange={(event) => handleHeightChange(event.target.value)}
         />
       </div>
-      <Image
-        className='rounded-lg'
-        src={imageSrc}
-        width={width || 400}
-        height={height || 400}
-        alt="smollkitten"
-        isBlurred
-        isZoomed
-        fallbackSrc="https://http.cat/status/202"
-      />
+      {imageSrc ? (
+        <Image
+          className='rounded-lg'
+    src={imageSrc}
+    width={width || 400}
+    height={height || 400}
+    alt="smollkitten"
+    isBlurred
+    isZoomed
+    fallbackSrc="https://http.cat/status/202"
+  />
+) : (
+  <span class="loader"></span>
+)}
     </main>
   );
 }
